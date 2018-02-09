6c5157c04bf57b8b06c9e648939907a1c96f86e2
daniel kelly <danieljkelly91@gmail.com>
2018-02-09 10:46:35 +0000

    save method added to customer class

--- models/customer.rb
+++ models/customer.rb
@@ -2,11 +2,18 @@ class Customer
 
   attr_reader :id
   attr_accessor :name, :funds
-  
+
   def initialize(options)
     @id = options['id'] if options['id']
     @name = options['name']
     @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers(name, funds) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run(sql, values)
+    @id = customer['id'].to_i
   end
 
 
