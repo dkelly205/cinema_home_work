265c992c9a911ed8f55a59490d658873f059b06a
daniel kelly <danieljkelly91@gmail.com>
2018-02-09 11:14:17 +0000

    Film.all method added to film class

--- models/film.rb
+++ models/film.rb
@@ -14,7 +14,15 @@ class Film
   def save()
     sql = "INSERT INTO films(title, price) VALUES ($1, $2) RETURNING id"
     values = [@title, @price]
-    film = SqlRunner.run(sql, values).first 
+    film = SqlRunner.run(sql, values).first
     @id = film['id'].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM films"
+    values = []
+    films = SqlRunner.run(sql, values)
+    result = films.map { |film| Film.new(film)}
+    return result
   end
 end
