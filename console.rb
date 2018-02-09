6e45ee3fb2062930349a73e1dec578d3645b6366
daniel kelly <danieljkelly91@gmail.com>
2018-02-09 11:07:36 +0000

    save method added to ticket class

--- console.rb
+++ console.rb
@@ -11,6 +11,8 @@ customer1.save
 film1 = Film.new({'title' => 'Den of Thieves', 'price' => 10})
 film1.save
 
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save
 
 binding.pry
 nil
